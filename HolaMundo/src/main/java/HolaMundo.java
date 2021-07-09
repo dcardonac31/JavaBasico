e023d8cf0c3c3d73e11bc4a2e236029797ebfc02
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-09 00:29:05 -0500

    Tipos Primitivos tipo char

--- HolaMundo/src/main/java/HolaMundo.java
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -5,22 +5,21 @@ public class HolaMundo {
 
     public static void main(String[] args) {
 
-        var floatVar = 1000.10F;
-        System.out.println("floatVar = " + floatVar);
+        System.out.println("bits tipo char:" + Character.SIZE);
+        System.out.println("bytes tipo char:" + Character.BYTES);
+        System.out.println("valor minimo tipo char:" + Character.MIN_VALUE);
+        System.out.println("valor maximo tipo char:" + Character.MAX_VALUE);
         
-        System.out.println("bits tipo float:" + Float.SIZE);
-        System.out.println("bytes tipo float:" + Float.BYTES);
-        System.out.println("valor minimo tipo float:" + Float.MIN_VALUE);
-        System.out.println("valor maximo tipo float:" + Float.MAX_VALUE);
+        var varChar = '\u0021';
+        System.out.println("varChar = " + varChar);
+
+        char varCharDecimal = 33;
+        System.out.println("varCharDecimal = " + varCharDecimal);
         
+        var varCharSimbolo = '!';
+        System.out.println("varCharSimbolo = " + varCharSimbolo);
+
         System.out.println("----------------------------");
 
-        var doubleVar = 100D;
-        System.out.println("doubleVar = " + doubleVar);
-        
-        System.out.println("bits tipo double:" + Double.SIZE);
-        System.out.println("bytes tipo double:" + Double.BYTES);
-        System.out.println("valor minimo tipo double:" + Double.MIN_VALUE);
-        System.out.println("valor maximo tipo double:" + Double.MAX_VALUE);
     }
 }
