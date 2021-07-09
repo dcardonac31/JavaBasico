ce964d7064ca8e36322b29678852f14344a833c7
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-08 23:47:29 -0500

    Tipos primitivos ripos Flotantes
    Please enter the commit message for your changes. Lines starting

--- HolaMundo/src/main/java/HolaMundo.java
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -5,17 +5,22 @@ public class HolaMundo {
 
     public static void main(String[] args) {
 
-        var numeroDecimal = 10;
-        System.out.println("numeroDecimal = " + numeroDecimal);
+        var floatVar = 1000.10F;
+        System.out.println("floatVar = " + floatVar);
         
-        var numeroOctal = 012;
-        System.out.println("numeroOctal = " + numeroOctal);
-
-        var numeroHexadecimal = 0xA;
-        System.out.println("numeroHexadecimal = " + numeroHexadecimal);
+        System.out.println("bits tipo float:" + Float.SIZE);
+        System.out.println("bytes tipo float:" + Float.BYTES);
+        System.out.println("valor minimo tipo float:" + Float.MIN_VALUE);
+        System.out.println("valor maximo tipo float:" + Float.MAX_VALUE);
         
-        var numeroBinario = 0b1010;
-        System.out.println("numeroBinario = " + numeroBinario);
+        System.out.println("----------------------------");
 
+        var doubleVar = 100D;
+        System.out.println("doubleVar = " + doubleVar);
+        
+        System.out.println("bits tipo double:" + Double.SIZE);
+        System.out.println("bytes tipo double:" + Double.BYTES);
+        System.out.println("valor minimo tipo double:" + Double.MIN_VALUE);
+        System.out.println("valor maximo tipo double:" + Double.MAX_VALUE);
     }
 }
