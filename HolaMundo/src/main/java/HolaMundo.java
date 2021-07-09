38efe5619f9e067c2f3b52f43cbb9cb9b9b8aa7e
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-08 23:41:20 -0500

    Base decimal, hexadecimal, binario, octal

--- HolaMundo/src/main/java/HolaMundo.java
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -5,45 +5,17 @@ public class HolaMundo {
 
     public static void main(String[] args) {
 
-        //byte, short, int, long
-        byte byteVar = 127;
-        System.out.println("byteVar = " + byteVar);
+        var numeroDecimal = 10;
+        System.out.println("numeroDecimal = " + numeroDecimal);
+        
+        var numeroOctal = 012;
+        System.out.println("numeroOctal = " + numeroOctal);
 
-        System.out.println("bits tipo byte: " + Byte.SIZE);
-        System.out.println("bytes tipo byte: " + Byte.BYTES);
-        System.out.println("valor minimo tipo byte: " + Byte.MIN_VALUE);
-        System.out.println("valor maximo tipo byte: " + Byte.MAX_VALUE);
-
-        System.out.println("--------------------------------------");
-
-        short shortVar = 1000;
-        System.out.println("shortVar = " + shortVar);
-
-        System.out.println("bits tipo short: " + Short.SIZE);
-        System.out.println("bytes tipo short: " + Short.BYTES);
-        System.out.println("valor minimo tipo short: " + Short.MIN_VALUE);
-        System.out.println("valor maximo tipo short: " + Short.MAX_VALUE);
-
-        System.out.println("--------------------------------------");
-
-        short intVar = 5000;
-        System.out.println("intVar = " + intVar);
-
-        System.out.println("bits tipo int: " + Integer.SIZE);
-        System.out.println("bytes tipo int: " + Integer.BYTES);
-        System.out.println("valor minimo tipo int: " + Integer.MIN_VALUE);
-        System.out.println("valor maximo tipo int: " + Integer.MAX_VALUE);
-
-        System.out.println("--------------------------------------");
-
-        long longVar = 5000000064546464451L;
-        System.out.println("longVar = " + longVar);
-
-        System.out.println("bits tipo long: " + Long.SIZE);
-        System.out.println("bytes tipo long: " + Long.BYTES);
-        System.out.println("valor minimo tipo long: " + Long.MIN_VALUE);
-        System.out.println("valor maximo tipo long: " + Long.MAX_VALUE);
-
+        var numeroHexadecimal = 0xA;
+        System.out.println("numeroHexadecimal = " + numeroHexadecimal);
+        
+        var numeroBinario = 0b1010;
+        System.out.println("numeroBinario = " + numeroBinario);
 
     }
 }
