eea2c8340f7f52d81bbbd2c959492fc4b67a8049
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-10 23:59:41 -0500

    Ejercicio de operadores condicionales.

--- HolaMundo/src/main/java/HolaMundo.java
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -7,37 +7,18 @@ public class HolaMundo {
 
         var scanner = new Scanner(System.in);
         
-        int a=3, b=3;
+        int a = 10;
+        int valorMinimo = 0, valorMaximo = 10;
+        boolean resultado = a >= valorMinimo && a <= valorMaximo;
+        System.out.println("resultado = " + resultado);
         
-        boolean c = a == b;
-        System.out.println("c = "+ c);
-        
-        c= (a !=b);
-        System.out.println("c = " + c);
-
-        
-        String cadena = new String();
-        String cadena2 = new String();        
-        cadena = "hola";
-        cadena2 = "hola";
-        System.out.println(cadena.equals(cadena2));
-        
-        boolean d = a <= b;
-        System.out.println("d = " + d);
-        
-        if(b % 2 == 0){
-            System.out.println("numero par");
-        }
-        else{
-            System.out.println("numero impar");
-        }
-        
-        int edad = 8;
-        int adulto = 18;
-        if(edad >= adulto)
-            System.out.println("es un adulto");
+        boolean vacaciones = false;
+        boolean diaDescanso = false;
+        if(vacaciones || diaDescanso)
+            System.out.println("Padre puede asistir al juego delm hijo");
         else
-            System.out.println("es menor de edad");
+            System.out.println("Pasdre ocupado");
+
         
 
         
