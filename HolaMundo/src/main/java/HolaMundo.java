11211fa8b465b56300230f644d9d03ba558e9e6a
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-11 00:03:09 -0500

    Ejercicio de operadores condicionales.

--- HolaMundo/src/main/java/HolaMundo.java
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -7,18 +7,8 @@ public class HolaMundo {
 
         var scanner = new Scanner(System.in);
         
-        int a = 10;
-        int valorMinimo = 0, valorMaximo = 10;
-        boolean resultado = a >= valorMinimo && a <= valorMaximo;
+       var resultado = (3 > 2) ? true : false;
         System.out.println("resultado = " + resultado);
-        
-        boolean vacaciones = false;
-        boolean diaDescanso = false;
-        if(vacaciones || diaDescanso)
-            System.out.println("Padre puede asistir al juego delm hijo");
-        else
-            System.out.println("Pasdre ocupado");
-
         
 
         
